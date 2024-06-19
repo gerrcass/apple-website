37229c335465496b89c9e6860215d79076b23c0f
Gerardo Castillo <gerrcass@gmail.com>
2024-06-19 10:42:43 -0400

    feat: Integrate Sentry.io for error tracking

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import Hero from "./components/Hero"
 import Hightlights from "./components/Hightlights"
 import Model from "./components/Model"
 
+//import * as Sentry from '@sentry/react'
+
 const App = () => {
+
+  // return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+
 
   return (
     <main className="bg-black">
@@ -16,3 +21,4 @@ const App = () => {
 }
 
 export default App
+// export default Sentry.withProfiler(App)
